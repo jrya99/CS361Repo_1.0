d2d0a86258b12117546b4ed7c55aa9b2414e77aa
jakeryan <jakeryan2899@gmail.com>
2022-04-25 20:02:58 -0500

    fixed css + started to make course model for db

--- PurplePanda/models.py
+++ PurplePanda/models.py
@@ -5,3 +5,8 @@ class User(models.Model):
     name = models.CharField(max_length=20)
     password = models.CharField(max_length=20)
     role = models.CharField(max_length=20)
+
+
+class Courses(models.Model):
+    courseName = models.CharField(max_length=20)
+
