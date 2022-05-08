6bc4602c36d8390ed83f0817204bd3c378447b6e
Mitchell Vang <80791092+Mitchell-Vang@users.noreply.github.com>
2022-05-08 15:16:05 -0500

    Update models.py
    added setters and added an assignments class

--- PurplePanda/models.py
+++ PurplePanda/models.py
@@ -10,7 +10,38 @@ class MyUser(models.Model):
 
     def __str__(self):
         return self.name, self.password, self.role
+    
+    def set_name(self, name):
+        self.name = name
 
+    def set_password(self, password):
+        self.password = password
+
+    def set_role(self, role):
+        self.role = role
+
+    def set_phoneNumber(self, phoneNumber):
+        self.phoneNumber = phoneNumber
+
+    def set_address(self, address):
+        self.address = address
+
+class Assignments(models.Model):
+    name = models.CharField(max_length=50)
+    date = models.CharField(max_length=50)
+    assignments = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_date(self, date):
+        self.date = date
+
+    def set_assignments(self, assignments):
+        self.assignments = assignments;
+
+    def __str__(self):
+        return self.name, self.date, self.assignments
 
 class MyCourses(models.Model):
     courseName = models.CharField(max_length=20)
