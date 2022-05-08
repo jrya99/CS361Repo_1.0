c5418e142d1f680b0c771d87817a86bef5bd0a84
Mitchell Vang <80791092+Mitchell-Vang@users.noreply.github.com>
2022-05-08 15:12:15 -0500

    Update AcceptanceTestTA.py

--- PurplePanda/AcceptanceTests/AcceptanceTestTA.py
+++ PurplePanda/AcceptanceTests/AcceptanceTestTA.py
@@ -6,51 +6,53 @@ from PurplePanda.assignment import Assignment
 
 class AcceptanceTA(TestCase):
 
-    def setUp(self):
+   def setUp(self):
         self.client = Client()
-        self.user1 = User.objects.create(name='Patrick', password='Star')
-        self.user2 = User.objects.create(name='Spongebob', password='Squarepants')
-        self.user3 = User.objects.create(name='Patrick', password='Stars')
-        self.assignment1 = Assignment.objects.create(name='Homework 1', date='11/5')
-        self.assignment2 = Assignment.objects.create(name='Homework 2', date='12/7')
-        self.assignment3 = Assignment.objects.create(name='Homework 3', date='bad due date')
-        self.list = {"assignment1": [self.assignment1.name, self.assignment1.date],
-                     "assignment2":[self.assignment2.name, self.assignment1.date]}
-        self.invalid_list = {"assignment1": [self.assignment1.name, self.assignment1.date],
-                     "assignment3":[self.assignment3.name, self.assignment3.date]}
+        self.user1 = User.objects.create(name='Patrick', password='Star', role='TA',
+                                         phoneNumber='2622622626', address='ChickFilA')
+        self.user2 = User.objects.create(name='Spongebob', password='Squarepants', role='TA',
+                                         phoneNumber='1234567890', address='Wendys')
+        self.user3 = User.objects.create(name='Patrick', password='Stars', role='TA',
+                                         phoneNumber='2622622626', address='McDonalds')
+        self.assignment1 = Assignments.objects.create(name='Homework 1', date='11/5')
+        self.assignment2 = Assignments.objects.create(name='Homework 2', date='12/7')
+        self.assignment3 = Assignments.objects.create(name='Homework 3', date='bad due date')
 
 
     def test_valid_login(self):
         response = self.client.post('/', {'name': 'Patrick', 'password': 'Star'})
-        self.assertEqual(response.url, "/things")
+        self.assertEqual(response.url, "/home")
 
     def test_valid_invalid_login(self):
         response = self.client.post('/', {"name": "Spongebob", "password": "Star"})
         self.assertEqual(response.context['message'], "Incorrect Password")
 
     def test_valid_view_assignments(self):
-   #     response = self.client.post('/', {'name': 'Spongebob', 'password': 'Squarepants'})
-   #     self.assertEqual(response.url, "/things")
+        response = self.client.post('/', {'name': 'Patrick', 'password': 'Star', 'role': 'TA'})
+        self.assertEqual(response.url, "/home")
 
-     #   response = self.client.get('/assignments/', {'name': self.assignment1.name, 'date': self.assignment1.date})
+        response = self.client.get('/assignments/', {'username': self.user1.username})
+        self.assertEqual(response.context['error'], 'No assignments exist yet...')
 
-      #  self.assertEqual(response.context['things'], [self.assignment1.name, self.assignment1.date])
+        response = self.client.get('/assignments/', {'username': self.user1.username})
+        all_assignments = []
 
-
-        for i in self.list.keys():
-            resp = self.client.post("/", {"name": i, "date": i})
-            self.assertEqual(resp.url, "/things/")
-
-            resp = self.client.post('/things/', {'name': self.assignment1.name})
-            self.assertEqual(len(resp.context['things']), len(self.thingList[i])+1)
-            self.assertEqual(resp.context['things'], self.thingList[i] + [self.assignment1.name])
+        for i in response.context['assignments']:
+            all_assignments.append(str(i))
+        self.assertEqual(all_assignments, [str(self.assignment1)])
 
     def test_invalid_view_assignments(self):
+        response = self.client.post('/', {'name': 'Patrick', 'password': 'Star', 'role': 'TA'})
+        self.assertEqual(response.url, "/home")
+
+        response = self.client.get('/assignments/', {'username': self.user1.username})
+        self.assertEqual(response.context['error'], 'No assignments exist yet...')
+
+
 
     def test_valid_obtain_info(self):
         response = self.client.post('/', {'name': 'Spongebob', 'password': 'Squarepants'})
-        self.assertEqual(response.url, "/things")
+        self.assertEqual(response.url, "/contactinfo")
 
         response = self.client.get('/', {'name': 'Spongebob', 'password': 'Squarepants'})
-
-        self.assertEqual(response.contect['things'], [self.user2.name])
+        self.assertEqual(response.context['contact_info'], [self.user2.name])
