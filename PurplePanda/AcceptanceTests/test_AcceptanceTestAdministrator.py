2fe553ef5fd8434a5334e7882e8daa2762d34938
Mitchell-Vang <vang638@uwm.edu>
2022-05-16 20:18:44 -0500

    updated instructor

--- PurplePanda/AcceptanceTests/test_AcceptanceTestAdministrator.py
+++ PurplePanda/AcceptanceTests/test_AcceptanceTestAdministrator.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.test import Client
-from PurplePanda.models import MyUser
+from PurplePanda.models import MyUser, MyCourses
 
 
 class AcceptanceAdmin(TestCase):
@@ -9,7 +9,7 @@ class AcceptanceAdmin(TestCase):
         self.client = Client()
         self.user1 = MyUser.objects.create(name='Patrick', password='Star', role='Administrator', phoneNumber='2622622626', address='ChickFilA')
         self.user2 = MyUser.objects.create(name='Sandy', password='Cheeks', role='TA', phoneNumber='1234567890', address='BikiniBottom')
-
+        self.course1 = MyCourses.objects.create(courseName='CS361', courseSection='101')
     def test_valid_login(self):
         response = self.client.post('/', {'name': 'Patrick', 'password': 'Star', 'role': 'Administrator'})
         self.assertEqual(response.url, "/home/")
@@ -20,7 +20,7 @@ class AcceptanceAdmin(TestCase):
 
     def test_create_user(self):
         response = self.client.post('/', {'name': 'Patrick', 'password': 'Star', 'role': 'Administrator'})
-        self.assertEqual(response.url, "/home")
+        self.assertEqual(response.url, "/home/")
 
         response = self.client.post('/create_users/', {'name': self.user2.username, 'password': None, 'role': 'TA'})
         all_users = {}
@@ -29,13 +29,31 @@ class AcceptanceAdmin(TestCase):
             all_users.update(str(i))
         self.assertEqual(all_users, {'name': self.user2.username, 'password': None, 'role': 'TA'})
 
-    def test__invalid_create_user(self):
+    def test_invalid_create_user(self):
         response = self.client.post('/', {'name': 'Patrick', 'password': 'Star', 'role': 'Administrator'})
         self.assertEqual(response.url, "/home/")
 
         response = self.client.post('/create_users/', {'name': self.user2.username, 'password': None, 'role': 'TA'})
-
         self.assertEqual(response.context['message'], "invalid username, password, or role")
+
+    def test_create_course(self):
+            response = self.client.post('/', {'name': 'Patrick', 'password': 'Star', 'role': 'Administrator'})
+            self.assertEqual(response.url, "/home/")
+
+            response = self.client.post('/create_course/', {'name': self.user2.username, 'password': None, 'role': 'TA'})
+            all_users = {}
+
+            for i in response.context['course']:
+                all_users.update(str(i))
+            self.assertEqual(all_users, {'courseName': self.course1.courseName, 'courseSection': self.course1.courseName})
+
+    def test_invalid_create_course(self):
+        response = self.client.post('/', {'name': 'Patrick', 'password': 'Star', 'role': 'Administrator'})
+        self.assertEqual(response.url, "/home/")
+
+        response = self.client.post('/create_course/', {'courseName': self.course1.courseName, 'courseSection': None})
+        self.assertEqual(response.context['message'], "invalid username, password, or role")
+
 
         #delete course and delete section
 
